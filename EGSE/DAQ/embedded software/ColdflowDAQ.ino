3dc282a2e7696b07d2791dff9559f45db22dfb50
angelina-z <139913232+angelina-z@users.noreply.github.com>
2026-04-11 01:17:15 -0700

    Update ColdflowDAQ.ino

--- EGSE/DAQ/embedded software/ColdflowDAQ.ino
+++ EGSE/DAQ/embedded software/ColdflowDAQ.ino
@@ -52,15 +52,13 @@ SPIClass sharedSPI(FSPI);
 ADS1256 loadCellADC(&sharedSPI, ADS1256_DRDY, ADS1256_CS, 2.5);
 ADS8688 pressureADC;
 
-float calibrationA1 = 0.256906;
-calibrationA1 = calibrationA1*-100000;
-float calibrationB1 = -6.391059;
+float calibrationA1 = 0.260906  * -100000; //don't delete this mult
+float calibrationB1 = -6.138861;
 float convertToWeightLC1(float voltage)
 {
   return (calibrationA1 * voltage) + calibrationB1;
 }
-float calibrationA2 = 1.722651;
-calibrationA2 = calibrationA2*-100000;
+float calibrationA2 = 1.722651 * -100000; //don't delete this mult
 float calibrationB2 = 11.209444;
 float convertToWeightLC2(float voltage)
 {
