32baf0af3ab2ed94c04a30e2711cc76622d1f129
polpolcharchar <jackfitterer876@gmail.com>
2026-04-28 19:49:59 -0700

    Rename to .cpp

---
+++ EGSE/DAQ/embedded software/PRODUCTION/HotfireDAQ/src/main.cpp
@@ -0,0 +1,288 @@
+#include <Arduino.h>
+#include <ADS1256.h>
+#include "ADS8688.h"
+#include <SPI.h>
+#include <string>
+#include <Adafruit_ADS1X15.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// Load Cell (ADS1256) SPI Pins
+#define ADS1256_MISO 35
+#define ADS1256_SCLK 48
+#define ADS1256_MOSI 34
+#define ADS1256_CS 7
+#define ADS1256_DRDY 4
+
+// PT (ADS8688) SPI
+#define ADS8688_CS 36
+
+// LED indicator pin
+#define LED 38
+#define RO_PIN 44    // 16
+#define DI_PIN 43    // 17
+#define DE_RE_PIN 41 // 23
+
+struct SensorData
+{
+  float pt[8];
+  float lc[2];
+  uint32_t timestamp;
+};
+
+QueueHandle_t sensorQueue;
+SemaphoreHandle_t spiMutex;
+
+unsigned long last_mqtt_attempt = 0;
+static unsigned long lastMqttLoop = 0;
+const unsigned long MQTT_RETRY_INTERVAL = 5000;
+
+const unsigned long SAMPLE_INTERVAL = 1;
+const unsigned long PUBLISH_INTERVAL = 4;
+
+const char *ssid = "ILAY";
+const char *password = "lebronpookie123";
+const char *mqtt_server = "192.168.0.100";
+const char *DAQ_TOPIC = "DAQ_transmitter/receiver";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+SPIClass sharedSPI(FSPI);
+ADS1256 loadCellADC(&sharedSPI, ADS1256_DRDY, ADS1256_CS, 2.5);
+ADS8688 pressureADC;
+
+
+float calibrationA1 = -1.722651;
+float calibrationB1 = 11.209444;
+float calibrationA2 = -0.260906;
+float calibrationB2 = -6.138861;
+int hardCodeConstant1 = 20; //constant is hardcoded value to account for difference in load cell mounting (should know m is same)
+float convertToWeightLC1(float voltage)
+{
+  return (voltage-calibrationB1)/calibrationA1 + hardCodeConstant1;
+}
+
+int hardCodeConstant2 = 52; //constant is hardcoded value to account for difference in load cell mounting (should know m is same)
+float convertToWeightLC2(float voltage)
+{
+  return (voltage-calibrationB2)/calibrationA2 + hardCodeConstant2;
+}
+
+void setup_wifi()
+{
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println("still connecting...");
+  }
+
+  WiFi.setSleep(false);
+
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void connect_client()
+{
+  if (client.connected())
+    return;
+  if (WiFi.status() != WL_CONNECTED)
+    return;
+
+  if (millis() - last_mqtt_attempt < MQTT_RETRY_INTERVAL)
+    return;
+  last_mqtt_attempt = millis();
+
+  String cid = "DAQ_TX-" + WiFi.macAddress();
+  if (client.connect(cid.c_str()))
+  {
+    Serial.println("Connected to MQTT broker");
+  }
+  else
+  {
+    Serial.println("MQTT connect failed");
+  }
+}
+
+float getCalibratedValue(float m, float b, float raw)
+{
+  return (raw - b) / m;
+}
+float mValues[8] = {2.59713, 8.187604, 0.036137, 8.142941, 0, 8.218279, 8.126287, 5.134613};
+float bValues[8] = {1005.1352, 1002.913757, 1350.183472, 1035.41272, 0, 999.9568237, 968.913, 1067.228};
+
+bool waitForDRDY(uint32_t timeout_us = 2000)
+{
+  uint32_t start = micros();
+  while (digitalRead(ADS1256_DRDY))
+  {
+    if (micros() - start > timeout_us)
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+void samplingTask(void *pvParameters)
+{
+  Serial.println("SAMPLING TASK");
+  TickType_t lastWakeTime = xTaskGetTickCount();
+
+  while (true)
+  {
+    float ptVoltages[8];
+    float ptCalibrated[8];
+    float lcCalibrated0 = 0.0;
+    float lcCalibrated1 = 0.0;
+    bool validSample = false;
+
+    if (xSemaphoreTake(spiMutex, portMAX_DELAY) == pdTRUE)
+    {
+      pressureADC.readAllChannels(ADS8688_CS, true, ptVoltages);
+      delayMicroseconds(5);
+
+      bool drdy0 = waitForDRDY();
+      float lcVoltage0 = drdy0
+        ? loadCellADC.convertToVoltage(loadCellADC.readDifferentialFaster(DIFF_0_1))*100000.0f
+        : 0.0f;
+
+      bool drdy1 = waitForDRDY();
+      float lcVoltage1 = drdy1
+        ? loadCellADC.convertToVoltage(loadCellADC.readDifferentialFaster(DIFF_2_3))*100000.0f
+        : 0.0f;
+
+      validSample = drdy0 && drdy1;
+
+      if (validSample)
+      {
+        lcCalibrated0 = convertToWeightLC1(lcVoltage0);
+        lcCalibrated1 = convertToWeightLC2(lcVoltage1);
+      }
+
+      xSemaphoreGive(spiMutex);
+    }
+
+    if (validSample)
+    {
+      for (int i = 0; i < 8; i++)
+      {
+        ptCalibrated[i] = getCalibratedValue(mValues[i], bValues[i], ptVoltages[i]);
+      }
+
+      SensorData newData;
+      for (int i = 0; i < 8; i++)
+        newData.pt[i] = ptCalibrated[i];
+
+      newData.lc[0] = lcCalibrated0;
+      newData.lc[1] = lcCalibrated1;
+      newData.timestamp = millis();
+
+      xQueueOverwrite(sensorQueue, &newData);
+    }
+
+    vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(SAMPLE_INTERVAL));
+  }
+}
+
+void publishTask(void *pvParameters)
+{
+  Serial.println("PUBLISH TASK");
+  char finalStr[300];
+  SensorData localCopy;
+
+  while (true)
+  {
+    if (!client.connected())
+    {
+      vTaskDelay(pdMS_TO_TICKS(100));
+      continue;
+    }
+
+    if (xQueuePeek(sensorQueue, &localCopy, 0) == pdTRUE)
+    {
+      snprintf(
+          finalStr,
+          sizeof(finalStr),
+          "rocket_data pt0=%4.2f,pt1=%4.2f,pt2=%4.2f,pt3=%4.2f,pt4=%4.2f,pt5=%4.2f,pt6=%4.2f,pt7=%4.2f,lc0=%4.10f,lc1=%4.10f,uptime_ms=%lu",
+          localCopy.pt[0],
+          localCopy.pt[1],
+          localCopy.pt[2],
+          localCopy.pt[3],
+          localCopy.pt[4],
+          localCopy.pt[5],
+          localCopy.pt[6],
+          localCopy.pt[7],
+          localCopy.lc[0],
+          localCopy.lc[1],
+          localCopy.timestamp);
+
+      client.publish(DAQ_TOPIC, finalStr);
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(PUBLISH_INTERVAL));
+  }
+}
+
+void mqttTask(void *pvParameters)
+{
+  while (true)
+  {
+    if (WiFi.status() != WL_CONNECTED)
+    {
+      WiFi.begin(ssid, password);
+      vTaskDelay(pdMS_TO_TICKS(500));
+      continue;
+    }
+
+    connect_client();
+    client.loop();
+    vTaskDelay(pdMS_TO_TICKS(10));
+  }
+}
+
+void setup()
+{
+  pinMode(DE_RE_PIN, OUTPUT);
+  digitalWrite(DE_RE_PIN, HIGH);
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10);
+  delay(2000);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
+
+  sensorQueue = xQueueCreate(1, sizeof(SensorData));
+  spiMutex = xSemaphoreCreateMutex();
+
+  sharedSPI.begin(ADS1256_SCLK, ADS1256_MISO, ADS1256_MOSI, -1);
+  if (xSemaphoreTake(spiMutex, portMAX_DELAY) == pdTRUE)
+  {
+    loadCellADC.InitializeADC();
+    loadCellADC.setPGA(PGA_64);
+    loadCellADC.setDRATE(DRATE_1000SPS);
+    pressureADC.begin(ADS1256_MISO, ADS1256_SCLK, ADS1256_MOSI, ADS8688_CS, 4.1, 0x05);
+    pressureADC.setInputRange(ADS8688_CS, 0x05);
+    xSemaphoreGive(spiMutex);
+  }
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setKeepAlive(60);
+
+  xTaskCreatePinnedToCore(samplingTask, "Sampling Task", 4096, NULL, 3, NULL, 1);
+  xTaskCreatePinnedToCore(publishTask, "Publish Task", 6144, NULL, 2, NULL, 1);
+  xTaskCreatePinnedToCore(mqttTask, "MQTT Task", 4096, NULL, 1, NULL, 0);
+
+  Serial.println("Setup complete");
+}
+
+void loop() {}
