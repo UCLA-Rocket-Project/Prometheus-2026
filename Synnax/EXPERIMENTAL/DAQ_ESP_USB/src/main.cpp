2e2916dd41f1751d91e2273285aba5ce52dd9394
R K <rikopelciw@gmail.com>
2026-05-19 19:33:46 -0700

    update for new daq

--- Synnax/EXPERIMENTAL/DAQ_ESP_USB/src/main.cpp
+++ Synnax/EXPERIMENTAL/DAQ_ESP_USB/src/main.cpp
@@ -4,22 +4,32 @@
 #include <SPI.h>
 #include <string>
 #include <Adafruit_ADS1X15.h>
+#include "Adafruit_MCP23X17.h"
 
-// Load Cell (ADS1256) SPI Pins
-#define ADS1256_MISO 35
-#define ADS1256_SCLK 48
-#define ADS1256_MOSI 34
+// SPI
+#define MISO 35
+#define MOSI 34
+#define SCLK 48
+
+// Load Cell SPI Pins
 #define ADS1256_CS 7
 #define ADS1256_DRDY 4
 
-// PT (ADS8688) SPI
+// PT SPI Pins
 #define ADS8688_CS 36
 
-// LED indicator pin
-#define LED 38
-#define RO_PIN 44    // 16
-#define DI_PIN 43    // 17
-#define DE_RE_PIN 41 // 23
+int logicalToLibraryPtIndex[8] = {
+  4,
+  5,
+  6,
+  7,
+  0,
+  1,
+  2,
+  3
+};
+
+
 
 struct SensorData
 {
@@ -31,64 +41,28 @@ struct SensorData
 QueueHandle_t sensorQueue;
 SemaphoreHandle_t spiMutex;
 
-const unsigned long SAMPLE_INTERVAL = 1;
-const unsigned long PUBLISH_INTERVAL = 4;
-
-const int PT_CHANNEL_COUNT = 8;
-const int LC_CHANNEL_COUNT = 2;
+const unsigned long SAMPLE_INTERVAL = 200;
+const unsigned long PUBLISH_INTERVAL = 200;
 
 SPIClass sharedSPI(FSPI);
 ADS1256 loadCellADC(&sharedSPI, ADS1256_DRDY, ADS1256_CS, 2.5);
 ADS8688 pressureADC;
 
 
+float calibrationA1 = -315692.6875;
+float calibrationB1 = 66.165405 + 11;
+float calibrationA2 = -48867.5625;
+float calibrationB2 = 2.676071 + 35;
 
-// HOW TO UPDATE:
-// Find the sensor you want (pt0..pt7 or lc0..lc1).
-// Replace only the numbers for m and b.
-// Calibration now uses DIRECT form: calibrated = raw * m + b.
-// If you want to disable one PT channel, set m = 0.0 (output becomes 0.0).
-struct LinearCalibration {
-  float m;
-  float b;
-};
-
-struct LoadCellCalibration {
-  float m;
-  float b;
-};
-
-LinearCalibration PT_CALIBRATION[PT_CHANNEL_COUNT] = {
-//{m, b}
-  {0.38504041f, -387.01766950f},    // pt0
-  {0.12213585f, -122.49172737f},    // pt1
-  {27.67246866f, -37362.90981542f}, // pt2
-  {0.12280575f, -127.15463860f},    // pt3
-  {0.0f, 0.0f},              // pt4 (disabled if m=0)
-  {0.12167998f, -121.67472335f},    // pt5
-  {0.12305743f, -119.23194443f},    // pt6
-  {0.19475664f, -207.84974447f}     // pt7
-};
-
-LoadCellCalibration LC_CALIBRATION[LC_CHANNEL_COUNT] = {
-//{m, b}
-  {-0.58050064f, 26.50708936f}, // lc0
-  {-3.83279802f, 28.47098572f}  // lc1
-};
-
-float applyLinearCalibration(float raw, const LinearCalibration& cal)
+float getCalibratedValue(float m, float b, float raw)
 {
-  if (cal.m == 0.0f)
-    return 0.0f;
-  return raw * cal.m + cal.b;
+  return raw * m + b;
 }
+float mValues[8] = {0.123015, 0.123638, 0.123036, 0.376017, 0.198563, 0.122571, 0.123015, 0.123015};
+float bValues[8] = {-129.502991, -129.675354, -122.230255, -346.989288, -210.341034, -130.974503, -129.502991, -129.502991};
 
-float applyLoadCellCalibration(float voltage, const LoadCellCalibration& cal)
-{
-  if (cal.m == 0.0f)
-    return 0.0f;
-  return voltage * cal.m + cal.b;
-}
+// float mValues[8] = {1, 1, 1, 1, 1, 1, 1, 1};
+// float bValues[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 bool waitForDRDY(uint32_t timeout_us = 2000)
 {
@@ -110,8 +84,9 @@ void samplingTask(void *pvParameters)
 
   while (true)
   {
-    float ptVoltages[PT_CHANNEL_COUNT];
-    float ptCalibrated[PT_CHANNEL_COUNT];
+    float ptVoltages[8];
+    float ptCalibrated[8];
+    float logicalPtVoltageOrdering[8];
     float lcCalibrated0 = 0.0;
     float lcCalibrated1 = 0.0;
     bool validSample = false;
@@ -119,24 +94,29 @@ void samplingTask(void *pvParameters)
     if (xSemaphoreTake(spiMutex, portMAX_DELAY) == pdTRUE)
     {
       pressureADC.readAllChannels(ADS8688_CS, true, ptVoltages);
+
+      for(int i = 0; i < 8; i++){
+        logicalPtVoltageOrdering[i] = ptVoltages[logicalToLibraryPtIndex[i]];
+      }
+
       delayMicroseconds(5);
 
       bool drdy0 = waitForDRDY();
       float lcVoltage0 = drdy0
-        ? loadCellADC.convertToVoltage(loadCellADC.readDifferentialFaster(DIFF_0_1))*100000.0f
+        ? loadCellADC.convertToVoltage(loadCellADC.readDifferentialFaster(DIFF_0_1))
         : 0.0f;
 
       bool drdy1 = waitForDRDY();
       float lcVoltage1 = drdy1
-        ? loadCellADC.convertToVoltage(loadCellADC.readDifferentialFaster(DIFF_2_3))*100000.0f
+        ? loadCellADC.convertToVoltage(loadCellADC.readDifferentialFaster(DIFF_2_3))
         : 0.0f;
 
       validSample = drdy0 && drdy1;
 
       if (validSample)
       {
-        lcCalibrated0 = applyLoadCellCalibration(lcVoltage0, LC_CALIBRATION[0]);
-        lcCalibrated1 = applyLoadCellCalibration(lcVoltage1, LC_CALIBRATION[1]);
+        lcCalibrated0 = getCalibratedValue(calibrationA1, calibrationB1, lcVoltage0);
+        lcCalibrated1 = getCalibratedValue(calibrationA2, calibrationB2, lcVoltage1);
       }
 
       xSemaphoreGive(spiMutex);
@@ -144,13 +124,13 @@ void samplingTask(void *pvParameters)
 
     if (validSample)
     {
-      for (int i = 0; i < PT_CHANNEL_COUNT; i++)
+      for (int i = 0; i < 8; i++)
       {
-        ptCalibrated[i] = applyLinearCalibration(ptVoltages[i], PT_CALIBRATION[i]);
+        ptCalibrated[i] = getCalibratedValue(mValues[i], bValues[i], logicalPtVoltageOrdering[i]);
       }
 
       SensorData newData;
-      for (int i = 0; i < PT_CHANNEL_COUNT; i++)
+      for (int i = 0; i < 8; i++)
         newData.pt[i] = ptCalibrated[i];
 
       newData.lc[0] = lcCalibrated0;
@@ -166,7 +146,7 @@ void samplingTask(void *pvParameters)
 
 void publishTask(void *pvParameters)
 {
-  Serial.println("SERIAL TX TASK");
+  Serial.println("SERIAL OUTPUT TASK");
   char finalStr[300];
   SensorData localCopy;
 
@@ -189,6 +169,7 @@ void publishTask(void *pvParameters)
           localCopy.lc[0],
           localCopy.lc[1],
           localCopy.timestamp);
+
       Serial.println(finalStr);
     }
 
@@ -196,42 +177,27 @@ void publishTask(void *pvParameters)
   }
 }
 
-void pinModeInit() {
-  pinMode(DE_RE_PIN, OUTPUT);
-  digitalWrite(DE_RE_PIN, HIGH);
-  pinMode(LED, OUTPUT);
-  digitalWrite(LED, HIGH);
-}
-
-void loadCellInit() {
-  loadCellADC.InitializeADC();
-  loadCellADC.setPGA(PGA_64);
-  loadCellADC.setDRATE(DRATE_1000SPS);
-  pressureADC.begin(ADS1256_MISO, ADS1256_SCLK, ADS1256_MOSI, ADS8688_CS, 4.1, 0x05);
-  pressureADC.setInputRange(ADS8688_CS, 0x05);
-  xSemaphoreGive(spiMutex); 
-}
-
 void setup()
 {
-  pinModeInit();
-
   Serial.begin(115200);
-  while (!Serial)
-    delay(10);
   delay(2000);
 
   sensorQueue = xQueueCreate(1, sizeof(SensorData));
   spiMutex = xSemaphoreCreateMutex();
 
-  sharedSPI.begin(ADS1256_SCLK, ADS1256_MISO, ADS1256_MOSI, -1);
+  sharedSPI.begin(SCLK, MISO, MOSI, -1);
   if (xSemaphoreTake(spiMutex, portMAX_DELAY) == pdTRUE)
   {
-    loadCellInit();
+    loadCellADC.InitializeADC();
+    loadCellADC.setPGA(PGA_64);
+    loadCellADC.setDRATE(DRATE_1000SPS);
+    pressureADC.begin(MISO, SCLK, MOSI, ADS8688_CS, 4.1, 0x05);
+    pressureADC.setInputRange(ADS8688_CS, 0x05);
+    xSemaphoreGive(spiMutex);
   }
 
   xTaskCreatePinnedToCore(samplingTask, "Sampling Task", 4096, NULL, 3, NULL, 1);
-  xTaskCreatePinnedToCore(publishTask, "Serial Tx Task", 6144, NULL, 2, NULL, 1);
+  xTaskCreatePinnedToCore(publishTask, "Serial Output Task", 6144, NULL, 2, NULL, 1);
 
   Serial.println("Setup complete");
 }
