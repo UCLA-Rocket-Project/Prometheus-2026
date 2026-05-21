caa7f26bb6d7aec10d47ead6a2c40c0261b0ae72
angelina-z <139913232+angelina-z@users.noreply.github.com>
2026-05-20 18:19:34 -0700

    logs in steutp

--- EGSE/DAQ/embedded software/PRODUCTION/DAQ calibration/lcpt_calibration/src/main.cpp
+++ EGSE/DAQ/embedded software/PRODUCTION/DAQ calibration/lcpt_calibration/src/main.cpp
@@ -1,9 +1,8 @@
-// daq
-#include <ADS1256.h>
+//daq
 #include <Arduino.h>
-#include <SPI.h>
-
+#include <ADS1256.h>
 #include "ADS8688.h"
+#include <SPI.h>
 #include "lcpt_calibration.h"
 // Load Cell (ADS1256) SPI Pins
 #define ADS1256_MISO 35
@@ -17,14 +16,15 @@
 #define LED 38
 
 static const int logicalToLibraryPtIndex[8] = {
-    4,
-    5,
-    6,
-    7,
-    0,
-    1,
-    2,
-    3};
+  4,
+  5,
+  6,
+  7,
+  0,
+  1,
+  2,
+  3
+};
 
 SPIClass sharedSPI(FSPI);
 // ADS1256 instance
@@ -33,37 +33,40 @@ ADS1256 loadCellADC(&sharedSPI, ADS1256_DRDY, ADS1256_CS, 2.5);
 ADS8688 pressureADC;
 
 void setup() {
-    Serial.begin(115200);
-    delay(1000);
-    pinMode(LED, OUTPUT);
-    digitalWrite(LED, HIGH);
-    // Start custom SPI bus
-    sharedSPI.begin(ADS1256_SCLK, ADS1256_MISO, ADS1256_MOSI, ADS1256_CS);
-    // Initialize ADS1256 (Load Cell)
-    loadCellADC.InitializeADC();
-    loadCellADC.setPGA(PGA_64);
-    loadCellADC.setMUX(DIFF_0_1);
-    loadCellADC.setDRATE(DRATE_1000SPS);
-    // Initialize ADS8688 (PTs)
-    pressureADC.begin(ADS1256_MISO, ADS1256_SCLK, ADS1256_MOSI, ADS8688_CS, 4.1, 0x05);
-    pressureADC.setInputRange(ADS8688_CS, 0x01);//this maybe should be 0x06 but i think it is correct
 
-    startCalibration();
+Serial.begin(115200);
+delay(1000);
+Serial.println("Serial Started.");
+pinMode(LED, OUTPUT);
+digitalWrite(LED, HIGH);
+// Start custom SPI bus
+sharedSPI.begin(ADS1256_SCLK, ADS1256_MISO, ADS1256_MOSI, ADS1256_CS);
+// Initialize ADS1256 (Load Cell)
+loadCellADC.InitializeADC();
+loadCellADC.setPGA(PGA_64);
+loadCellADC.setMUX(DIFF_0_1);
+loadCellADC.setDRATE(DRATE_1000SPS);
+// Initialize ADS8688 (PTs)
+pressureADC.begin(ADS1256_MISO, ADS1256_SCLK, ADS1256_MOSI, ADS8688_CS, 4.1, 0x05);
+pressureADC.setInputRange(ADS8688_CS, 0x05);
+Serial.println("Setup Completed.");
+startCalibration();
 }
 
 float getLCValue(int channel) {
-    if (channel == 1) {
-    return loadCellADC.convertToVoltage(loadCellADC.readDifferentialFaster(DIFF_2_3));
+  if (channel == 1) {
+    return loadCellADC.convertToVoltage(loadCellADC.readDifferentialFaster(DIFF_2_3)) * 100000.0f;
   }
-  return loadCellADC.convertToVoltage(loadCellADC.readDifferentialFaster(DIFF_0_1));
+  return loadCellADC.convertToVoltage(loadCellADC.readDifferentialFaster(DIFF_0_1)) * 100000.0f;
 }
+
 float getPTValue(int correctChannel) {
-    float voltages[8];
-    pressureADC.readAllChannels(ADS8688_CS, true, voltages);
-    if (correctChannel < 0 || correctChannel > 7) return 0.0f;
-    return voltages[logicalToLibraryPtIndex[correctChannel]];
+  float voltages[8];
+  pressureADC.readAllChannels(ADS8688_CS, true, voltages);
+  if (correctChannel < 0 || correctChannel > 7) return 0.0f;
+  return voltages[logicalToLibraryPtIndex[correctChannel]];
 }
 
 void loop() {
-    delay(10000000);
+delay(10000000);
 }
